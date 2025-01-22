c3824fdbb85f0292bce8ccb5db91abb5851e1cf2
Joseph312577 <163013500+Joseph312577@users.noreply.github.com>
2025-01-22 13:41:10 +0000

    1/22/2025

---
+++ script.js
@@ -0,0 +1,55 @@
+// Select all images in the gallery
+const galleryImages = document.querySelectorAll('#gallery-container img');
+const images = Array.from(galleryImages); // convert nodeList to array
+// Select elements for lightbox
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+let currentIndex = 0; //Track the currently displayed image
+
+//open lightbox and synchronize curretnIndex when an image is clicked
+images.forEach((image, index) => {        
+    image.addEventListener('click', () => {
+        currentIndex = index;
+        lightboxImg.src = image.src;
+        lightbox.classList.add('visible');
+    });
+});
+//close lightbox when clicking outside the content
+lightbox.addEventListener('click', (event) => {
+    if (event.target === lightbox) {
+    lightbox.classList.remove('visible');
+    }
+});
+
+//show next image in the lightbox
+document.getElementById('next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+});
+
+
+
+
+
+//show previous image in the lightbox
+document.getElementById('prev').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+});
+
+
+
+//Keyboard navigation for the lightbox
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+        currentIndex = (currentIndex + 1) % images.length;
+        lightboxImg.src = images[currentIndex].src;
+    } else if (event.key === 'ArrowLeft') {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        lightboxImg.src = images[currentIndex].src;
+    } else if (event.key === 'Escape') {
+        lightbox.classList.remove('visible');
+    }
+})
